5c8c6b1d5b2e40a2d147c59fc15ed723b918b278
--unset <jocelinxy@gmail.com>
2023-03-01 16:22:21 +0800

    Define Auth Interface and Model Schema

--- src/shared/globals/decorators/joi-validation.decorators.ts
+++ src/shared/globals/decorators/joi-validation.decorators.ts
@@ -23,3 +23,20 @@ export function joiValidation(schema: ObjectSchema): IJoiDecorator {
     return descriptor;
   };
 }
+
+/**
+ * This code is implementing a decorator function called joiValidation, which is used to validate the request body of an HTTP endpoint using the Joi library.
+
+Here is how it works:
+
+The joiValidation function takes an argument schema, which is an instance of a Joi ObjectSchema.
+The joiValidation function returns another function of type IJoiDecorator. This returned function is a decorator that will be used to decorate a method in a class.
+The decorator function takes three arguments: target, key, and descriptor. These arguments are used by TypeScript to provide information about the decorated method.
+The originalMethod variable is used to store a reference to the original method that will be decorated.
+The decorator function then replaces the original method with a new function that performs the validation using the schema passed to the joiValidation function.
+The new function takes the arguments of the original method, which in this case are req, res, and next.
+The function then extracts the req object and validates its body property using the schema.validate method.
+If there is an error in validation, the function returns a new instance of a custom error class called JoiRequestValidationError, passing in the error message as a parameter.
+If there is no error, the function invokes the original method using originalMethod.apply(this, args), where this is the context of the class instance and args are the arguments passed to the function.
+Overall, this decorator function is used to add validation to HTTP endpoints by decorating methods that handle requests. If the request body fails validation, it will return an error. Otherwise, it will proceed with the original method.
+ */
